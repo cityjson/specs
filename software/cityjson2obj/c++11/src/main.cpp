777d99c8c3e10e19a1f752c1bae9fb93bb94d541
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-06-22 15:18:57 +0200

    cityjson2obj supports for all geom types

--- software/cityjson2obj/c++11/src/main.cpp
+++ software/cityjson2obj/c++11/src/main.cpp
@@ -19,6 +19,14 @@
 
 using json = nlohmann::json;
 
+void write_surface(std::ofstream &output, std::vector<std::vector<int> > &onesurface) {
+  output << "f";
+  for (auto& v: onesurface[0]) {
+    output << " " << int(v) + 1;
+  }
+  output << std::endl;
+}
+
 int main(int argc, const char * argv[]) {
   const char* filename = (argc > 1) ? argv[1] : "../../../../example-datasets/Munich/LOD2_4424_5482_solid.json";
   std::ifstream input(filename);
@@ -27,10 +35,10 @@ int main(int argc, const char * argv[]) {
 
   std::ofstream output("out.obj", std::ifstream::out);
 
+  //-- 1. process vertices
   if (j.count("transform") == 0) {
-    for (auto& v : j["vertices"]) {
+    for (auto& v : j["vertices"]) 
       output << "v " << v[0] << " " << v[1] << " " << v[2] <<std::endl;
-    }
   }
   else {
     for (auto& v : j["vertices"]) {
@@ -40,20 +48,37 @@ int main(int argc, const char * argv[]) {
       output << std::fixed << std::setprecision(6) << "v " << x << " " << y << " " << z << std::endl;
     } 
   }
+
+  //-- 2. process faces
   for (json::iterator it = j["CityObjects"].begin(); it != j["CityObjects"].end(); ++it) {
     output << "o " << it.key() << std::endl;
     for (auto& g : it.value()["geometry"]) {
+      // std::cout << g["type"] << std::endl;
       if (g["type"] == "Solid") {
         for (auto& shell : g["boundaries"]) {
-          for (auto& surface : shell) {
-            output << "f";
-            for (auto& v: surface[0]) {
-              output << " " << int(v) + 1;
-            }
-            output << std::endl;
-          } 
+          for (auto& surface : shell) { 
+            // std::cout << surface << std::endl;
+            std::vector< std::vector<int> > t = surface;
+            write_surface(output, t);
+          }
         }
       }
+      else if ( (g["type"] == "MultiSurface") || (g["type"] == "CompositeSurface") ) {
+        for (auto& surface : g["boundaries"]) { 
+          std::vector< std::vector<int> > t = surface;
+          write_surface(output, t);
+        }
+      }
+      else if ( (g["type"] == "MultiSolid") || (g["type"] == "CompositeSolid") ) {
+        for (auto& solid : g["boundaries"]) {
+          for (auto& shell : solid) {
+            for (auto& surface : shell) { 
+              std::vector< std::vector<int> > t = surface;
+              write_surface(output, t);
+            }
+          }
+        }
+      }            
     }
   }
   output.close();
