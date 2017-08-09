fb70db5ca23d0dc4452919e1e937687ec960c822
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-08-09 17:40:21 +0200

    adding python script to OFF —> CityJSON

---
+++ software/off2cityjson/python/off2cityjson.py
@@ -0,0 +1,48 @@
+import sys
+import StringIO
+import json
+
+def main():
+  fIn = open(sys.argv[1])
+  cm = convert(fIn)
+  # json_str = json.dumps(cm)
+  json_str = json.dumps(cm, indent=2)
+  outname = sys.argv[1][:-3] + "json"
+  f = open(outname, "w")
+  f.write(json_str)
+  print "\nDone, output written to:", outname
+
+def convert(fIn):
+  #-- skip the header
+  l = fIn.next()
+  while (len(l) == 0) or (l[0] == '#') or (l[:3] == 'OFF'):
+    l = fIn.next()
+  numVertices = int(l.split()[0])
+  numFaces    = int(l.split()[1])
+  lstVertices = []
+  for i in range(numVertices):
+    lstVertices.append(map(float, fIn.next().split()))
+  lstFaces = []
+  for i in range(numFaces):
+    lstFaces.append(map(int, fIn.next().split()[1:]))
+  cm = {}
+  cm["type"] = "CityModel"
+  cm["version"] = "http://www.cityjson.org/version/0.1"
+  cm["CityObjects"] = {}
+  cm["vertices"] = []
+  for v in lstVertices:
+    cm["vertices"].append(v)
+  g = {'type': 'Solid'}
+  shell = []
+  for f in lstFaces:
+    shell.append([f])
+  g['boundaries'] = [shell]
+  g['lod'] = 1
+  o = {'type': 'GenericCityObject'}
+  o['geometry'] = [g]
+  cm["CityObjects"]["id-1"] = o
+  return cm
+
+
+if __name__ == "__main__":
+    main()  
