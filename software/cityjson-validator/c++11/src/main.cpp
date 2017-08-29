fd3aeb3003d5b078b5327c42ef8a9a940f64784b
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-08-29 12:00:28 +0200

    okay a bit of structure in that code
    it’s working and i’m getting there

--- software/cityjson-validator/c++11/src/main.cpp
+++ software/cityjson-validator/c++11/src/main.cpp
@@ -3,11 +3,92 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <set>
 
 using nlohmann::json;
 using nlohmann::json_uri;
 using nlohmann::json_schema_draft4::json_validator;
+
+static void loader(const json_uri &uri, json &schema);
+bool is_valid_json_schema(json& j, json& jschema, std::stringstream& ss);
+
+
+int main(int argc, char *argv[]) {
+  const char* nameschema = (argc > 2) ? argv[2] : "../../../../schema/cityjson-v02.schema.json";
+  std::ifstream input(nameschema);
+  std::cout << "Schema: " << nameschema << std::endl;
+  json jschema;
+  input >> jschema;
+  
+  json j;
+  const char* nameinput = (argc > 1) ? argv[1] : "/Users/hugo/temp/example2.json";
+  std::ifstream input2(nameinput);
+  input2 >> j;
+  std::stringstream ss;
+  if (is_valid_json_schema(j, jschema, ss) == false) {
+    std::cout << ss.str() << std::endl;
+    return 1;
+  }
+
+
+  //-- 2. More than just with the schema
+  std::cout << "---" << std::endl;
+  json tmp;
+  
+  tmp = jschema["properties"]["metadata"]["properties"];
+  for (json::iterator it = j["metadata"].begin(); it != j["metadata"].end(); ++it) {
+    if (tmp.find(it.key()) == tmp.end()) 
+      std::cout << "Metadata " << it.key() << " not CityJSON" << std::endl;
+  }
+
+  // tmp = jschema["definitions"]["Building"]["properties"]["attributes"]["properties"];
+  for (json::iterator coit = j["CityObjects"].begin(); coit != j["CityObjects"].end(); ++coit) {
+    std::string cotype = coit.value()["type"];
+    tmp = jschema["definitions"][cotype]["properties"]["attributes"]["properties"];
+    for (json::iterator it = coit.value()["attributes"].begin(); it != coit.value()["attributes"].end(); ++it) {
+      if (tmp.find(it.key()) == tmp.end()) {
+        std::cout << "Attributes '" << it.key() << "' not CityGML compliant." << std::endl;
+        std::cout << "\tCO: " << cotype << std::endl;
+      }
+    }
+    if (coit.value().count("Parts") > 0) {
+      for (auto& pid : coit.value()["Parts"]) {
+        if (j["CityObjects"].count(pid) == 0)
+        std::cout << pid << " doesnt exist." << std::endl;
+      }
+    }
+  }
+
+  //-- 3. "Conformance requirements"
+  std::cout << "Document is valid" << std::endl;
+  return 1;
+}
+
+
+bool is_valid_json_schema(json& j, json& jschema, std::stringstream& ss)
+{
+  //-- 1. Schema validation with http://json-schema.org
+  json_validator validator(loader, [](const std::string &, const std::string &) {});
+  try {
+    validator.set_root_schema(jschema);
+  } 
+  catch (std::exception& e) {
+    ss << "ERROR: CityJSON schema invalid." << std::endl;
+    ss << e.what() << std::endl;
+    return false;
+  }
+  try {
+    validator.validate(j);
+  } 
+  catch (std::exception& e) {
+    ss << "ERROR: Schema validation failed." << std::endl;
+    ss << e.what() << " at offset: " << std::cin.tellg() << std::endl;
+    return false;
+  }
+  return true;
+}
+
 
 static void loader(const json_uri &uri, json &schema) {
   std::fstream lf("." + uri.path());
@@ -19,73 +100,4 @@ static void loader(const json_uri &uri, json &schema) {
   catch (std::exception &e) {
     throw e;
   }
-}
-
-int main(int argc, char *argv[]) {
-  const char* nameschema = (argc > 2) ? argv[2] : "../../../../schema/cityjson-v02.schema.json";
-  std::ifstream input(nameschema);
-  json jschema;
-  input >> jschema;
-  json_validator validator(loader, [](const std::string &, const std::string &) {});
-  std::cout << "Schema: " << nameschema << std::endl;
-  try {
-    validator.set_root_schema(jschema);
-  } 
-  catch (std::exception &e) {
-    std::cerr << "setting root schema failed\n";
-    std::cerr << e.what() << "\n";
-  }
-
-  //-- 1. Schema validation with http://json-schema.org
-  json j;
-  const char* nameinput = (argc > 1) ? argv[1] : "/Users/hugo/temp/example2.json";
-  std::ifstream input2(nameinput);
-  input2 >> j;
-  std::cout << "Input file: " << nameinput << std::endl;
-  std::cout << "-----" << std::endl;
-  try {
-    validator.validate(j);
-  } 
-  catch (std::exception &e) {
-    std::cerr << "schema validation failed\n";
-    std::cerr << e.what() << " at offset: " << std::cin.tellg() << "\n";
-    return EXIT_FAILURE;
-  }
-
-  std::cout << "---" << std::endl;
-  json tmp;
-  
-  tmp = jschema["properties"]["metadata"]["properties"];
-  for (json::iterator it = j["metadata"].begin(); it != j["metadata"].end(); ++it) {
-    if (tmp.find(it.key()) == tmp.end()) 
-      std::cout << "Metadata " << it.key() << " not CityJSON" << std::endl;
-  }
-
-  //-- 2. More than just with the schema
-  tmp = jschema["definitions"]["Building"]["properties"]["attributes"]["properties"];
-  // for (json::iterator it = tmp.begin(); it != tmp.end(); ++it) 
-  //   possible.insert(it.key());
-  if (tmp.find("hugo") != tmp.end()) 
-    std::cout << "hugo present" << std::endl;
-  if (tmp.find("usage") != tmp.end()) 
-    std::cout << "usage present" << std::endl;
-
-  for (auto& co : j["CityObjects"]) {
-    if (co["type"] == "Building") {
-      for (json::iterator it = co["attributes"].begin(); it != co["attributes"].end(); ++it) {
-        if (tmp.find(it.key()) == tmp.end()) 
-          std::cout << "Attributes " << it.key() << " not CityGML" << std::endl;
-      }
-      if (co.find("Parts") != co.end()) {
-        for (auto& pid : co["Parts"]) {
-          if (j["CityObjects"].count(pid) == 0)
-          std::cout << pid << " doesnt exist." << std::endl;
-        }
-      }
-    }
-  }
-
-  //-- 3. "Conformance requirements"
-  std::cout << "Document is valid" << std::endl;
-  return 1;
 }
