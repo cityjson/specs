6dc7d7c18b98357e6eb5e45fa2afc966ce0205d7
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-08-29 09:33:38 +0200

    cleaning code

--- software/cityjson-validator/c++11/src/main.cpp
+++ software/cityjson-validator/c++11/src/main.cpp
@@ -8,60 +8,53 @@ using nlohmann::json;
 using nlohmann::json_uri;
 using nlohmann::json_schema_draft4::json_validator;
 
-
-
-static void loader(const json_uri &uri, json &schema)
-{
+static void loader(const json_uri &uri, json &schema) {
   std::fstream lf("." + uri.path());
   if (!lf.good())
     throw std::invalid_argument("could not open " + uri.url() + " tried with " + uri.path());
-
   try {
     lf >> schema;
-  } catch (std::exception &e) {
+  } 
+  catch (std::exception &e) {
     throw e;
   }
 }
 
-int main(int argc, char *argv[])
-{
-
-  const char* nameschema = argv[2];
+int main(int argc, char *argv[]) {
+  const char* nameschema = (argc > 2) ? argv[2] : "../../../../schema/cityjson.schema.json";
   std::ifstream input(nameschema);
   json jschema;
   input >> jschema;
-
-
-  // 2) create the validator and
   json_validator validator(loader, [](const std::string &, const std::string &) {});
-
+  std::cout << "Schema: " << nameschema << std::endl;
   try {
-    // insert this schema as the root to the validator
-    // this resolves remote-schemas, sub-schemas and references via the given loader-function
     validator.set_root_schema(jschema);
-  } catch (std::exception &e) {
+  } 
+  catch (std::exception &e) {
     std::cerr << "setting root schema failed\n";
     std::cerr << e.what() << "\n";
   }
 
-  std::cout << "-----here" << std::endl;
-  
-  // 3) do the actual validation of the document
+  //-- 1. Schema validation with http://json-schema.org
   json document;
-  const char* nameinput = argv[1];
+  const char* nameinput = (argc > 1) ? argv[1] : "/Users/hugo/temp/example2.json";
   std::ifstream input2(nameinput);
   input2 >> document;
-
+  std::cout << "Input file: " << nameinput << std::endl;
+  std::cout << "-----" << std::endl;
   try {
-    // std::cin >> document;
     validator.validate(document);
-  } catch (std::exception &e) {
+  } 
+  catch (std::exception &e) {
     std::cerr << "schema validation failed\n";
     std::cerr << e.what() << " at offset: " << std::cin.tellg() << "\n";
     return EXIT_FAILURE;
   }
 
-  std::cerr << "document is valid\n";
+  //-- 2. More than just with the schema
 
-  return EXIT_SUCCESS;
+
+  //-- 3. "Conformance requirements"
+  std::cout << "Document is valid" << std::endl;
+  return 1;
 }
