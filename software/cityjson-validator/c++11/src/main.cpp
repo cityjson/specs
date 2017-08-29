004c39fa381a2988123c3431cf910a7bdab2382b
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-08-29 10:52:40 +0200

    WIP testing how to validate in paranoid mode a cityjson file

--- software/cityjson-validator/c++11/src/main.cpp
+++ software/cityjson-validator/c++11/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <set>
 
 using nlohmann::json;
 using nlohmann::json_uri;
@@ -21,7 +22,7 @@ static void loader(const json_uri &uri, json &schema) {
 }
 
 int main(int argc, char *argv[]) {
-  const char* nameschema = (argc > 2) ? argv[2] : "../../../../schema/cityjson.schema.json";
+  const char* nameschema = (argc > 2) ? argv[2] : "../../../../schema/cityjson-v02.schema.json";
   std::ifstream input(nameschema);
   json jschema;
   input >> jschema;
@@ -36,14 +37,14 @@ int main(int argc, char *argv[]) {
   }
 
   //-- 1. Schema validation with http://json-schema.org
-  json document;
+  json j;
   const char* nameinput = (argc > 1) ? argv[1] : "/Users/hugo/temp/example2.json";
   std::ifstream input2(nameinput);
-  input2 >> document;
+  input2 >> j;
   std::cout << "Input file: " << nameinput << std::endl;
   std::cout << "-----" << std::endl;
   try {
-    validator.validate(document);
+    validator.validate(j);
   } 
   catch (std::exception &e) {
     std::cerr << "schema validation failed\n";
@@ -51,8 +52,38 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  //-- 2. More than just with the schema
+  std::cout << "---" << std::endl;
+  json tmp;
+  
+  tmp = jschema["properties"]["metadata"]["properties"];
+  for (json::iterator it = j["metadata"].begin(); it != j["metadata"].end(); ++it) {
+    if (tmp.find(it.key()) == tmp.end()) 
+      std::cout << "Metadata " << it.key() << " not CityJSON" << std::endl;
+  }
 
+  //-- 2. More than just with the schema
+  tmp = jschema["definitions"]["Building"]["properties"]["attributes"]["properties"];
+  // for (json::iterator it = tmp.begin(); it != tmp.end(); ++it) 
+  //   possible.insert(it.key());
+  if (tmp.find("hugo") != tmp.end()) 
+    std::cout << "hugo present" << std::endl;
+  if (tmp.find("usage") != tmp.end()) 
+    std::cout << "usage present" << std::endl;
+
+  for (auto& co : j["CityObjects"]) {
+    if (co["type"] == "Building") {
+      for (json::iterator it = co["attributes"].begin(); it != co["attributes"].end(); ++it) {
+        if (tmp.find(it.key()) == tmp.end()) 
+          std::cout << "Attributes " << it.key() << " not CityGML" << std::endl;
+      }
+      if (co.find("Parts") != co.end()) {
+        for (auto& pid : co["Parts"]) {
+          if (j["CityObjects"].count(pid) == 0)
+          std::cout << pid << " doesnt exist." << std::endl;
+        }
+      }
+    }
+  }
 
   //-- 3. "Conformance requirements"
   std::cout << "Document is valid" << std::endl;
