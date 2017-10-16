37788fa6d4f7552d3bd75cdcbe8bca793329c1b1
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-10-16 17:34:10 +0200

    fixed clang warning

--- software/cityjson-compress/c++11/src/Point3.cpp
+++ software/cityjson-compress/c++11/src/Point3.cpp
@@ -26,11 +26,11 @@ void Point3::translate(double x, double y, double z) {
 }
 
 std::array<double,3> Point3::get_array_double() {
-  return {_x, _y, _z};
+  return {{_x, _y, _z}};
 }
 
 std::array<int,3> Point3::get_array_int(int precision) {
-  return {int(_x * pow(10, precision)), int(_y * pow(10, precision)), int(_z * pow(10, precision))};
+  return {{int(_x * pow(10, precision)), int(_y * pow(10, precision)), int(_z * pow(10, precision))}};
 }
 
 
