dde793a60d32c0e34062abc4ec16a622ca45b8f9
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-12-07 11:24:02 +0100

    WIP writing “standard” CityGML from CityJSON

--- software/cityjson2citygml/c++11/src/main.cpp
+++ software/cityjson2citygml/c++11/src/main.cpp
@@ -8,13 +8,14 @@
 using json = nlohmann::json;
 
 json j;
+json jschema;
 
 
-void get_xml_header() {
+void xml_header() {
   std::cout << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 }
 
-void get_citygml_namespaces() {
+void citygml_header() {
   //-- boiler template for all files
   std::cout << "<CityModel xmlns=\"http://www.opengis.net/citygml/2.0\"\n";
   std::cout << "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n";
@@ -55,7 +56,7 @@ void get_citygml_namespaces() {
 }
 
 
-void get_citygml_metadata() {
+void metadata() {
   if (j.count("metadata") != 0) {
     json jm = j["metadata"];
     if (jm.count("bbox") != 0) {
@@ -77,16 +78,121 @@ void get_citygml_metadata() {
 }
 
 
+void surface() {
+  std::cout << "<gml:Polygon>" << std::endl;
+
+
+  std::cout << "</gml:Polygon>" << std::endl;
+}
+
+
+void shell(json& js) {
+  std::cout << "<gml:CompositeSurface>" << std::endl;
+  std::cout << "<gml:surfaceMember>" << std::endl;
+
+
+  std::cout << "</gml:surfaceMember>" << std::endl;
+  std::cout << "</gml:CompositeSurface>" << std::endl;
+}
+
+
+void solid(json& js) {
+  std::cout << "<gml:Solid>" << std::endl;
+  int numshell = 0;
+  for (auto& sh : js["boundaries"]) {
+    if (numshell == 0) 
+      std::cout << "<gml:exterior>" << std::endl;
+    else
+      std::cout << "<gml:interior>" << std::endl;
+    shell(sh);
+    if (numshell == 0) 
+      std::cout << "</gml:exterior>" << std::endl;
+    else
+      std::cout << "</gml:interior>" << std::endl;
+    numshell++;
+  }
+
+  
+  std::cout << "</gml:Solid>" << std::endl;
+}
+
+
+void building(std::string id, json& jb) {
+  std::cout << "<cityObjectMember>" << std::endl;
+  std::cout << "<bldg:Building gml:id=\"" << id << "\">" << std::endl;
+  //-- 1. attributes
+  json tmp;
+  std::string cotype = jb["type"];
+  tmp = jschema["definitions"][cotype]["properties"]["attributes"]["properties"];
+  for (json::iterator it = jb["attributes"].begin(); it != jb["attributes"].end(); ++it) {
+    if (tmp.find(it.key()) != tmp.end()) {
+      std::cout << "<" << it.key() << ">";
+      std::cout << it.value();
+      std::cout << "</" << it.key() << ">" << std::endl;
+    }
+    else {
+      // std::cout << "ATTRIBUTE GENERICS" << std::endl;
+      std::cout << "<gen:stringAttribute name=\"" << it.key() << ">" << std::endl;
+      std::cout << "<gen:value>" << it.value() << "</gen:value>" << std::endl;
+      std::cout << "</gen:stringAttribute>" << std::endl;
+    }
+  }
+  // 2. each geoms
+  for (auto& g : jb["geometry"]) {
+    // std::cout << g["type"] << std::endl;
+    int lod = g["lod"].get<int>();
+    std::cout << "<bldg:lod" << lod << g["type"].get<std::string>() << ">" << std::endl;
+    if (g["type"] == "Solid") {
+      solid(g);
+    }
+    std::cout << "</bldg:lod" << lod << g["type"].get<std::string>() << ">" << std::endl;
+  }
+
+
+  // 3. BuildingParts?
+  // 4. BuildingInstallations?
+
+
+
+  std::cout << "</bldg:Building>" << std::endl;
+  std::cout << "</cityObjectMember>" << std::endl;
+}
+
+
 int main(int argc, const char * argv[]) {
+  //-- the input file
   const char* filename = argv[1];
   std::ifstream input(filename);
   input >> j;
+  //-- the schema
+  std::string v = j["version"];
+  if (v.find("http://www.cityjson.org/version/") != std::string::npos)
+      v = v.substr(v.size() - 3);
+  std::string nameschema = "../../../../schema/cityjson-v" + v.substr(0, 1) + v.substr(2) + ".schema.json";
+  std::ifstream inputs(nameschema);
+  std::cout << "Schema: " << nameschema << std::endl << std::endl;
+  if (inputs.is_open() == true) {
+    inputs >> jschema;
+  }
+  else {
+    std::cout << "ERROR:   cannot find schema v" << v << std::endl << std::endl;  
+    return 0;
+  }
+
 
   std::cout << std::setprecision(3) << std::fixed;
-  get_xml_header();
-  get_citygml_namespaces();
+  xml_header();
   std::cout << "<!-- Automatically converted from CityJSON (http://www.cityjson.org) -->\n";
-  get_citygml_metadata();
+  citygml_header();
+  metadata();
+  
+  for (json::iterator coit = j["CityObjects"].begin(); coit != j["CityObjects"].end(); ++coit) {
+    if (coit.value()["type"] == "Building") {
+      // std::cout << "BUILDING" << std::endl;
+      building(coit.key(), coit.value());
+    }
+  }
+
   std::cout << "</CityModel>" << std::endl;
 
 
