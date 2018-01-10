2c4787edfb7b1f2fc36c2f3e7cd935bba551f73a
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-01-10 10:08:34 +0100

    WIP for textures

--- software/cjslicer/python/cjslicer.py
+++ software/cjslicer/python/cjslicer.py
@@ -1,7 +1,6 @@
 import sys
-import StringIO
 import json
-import copy
+
 
 def main():
   # fIn = open(sys.argv[1])
@@ -26,7 +25,7 @@ def main():
   newvertices = []    
   for each in j2["CityObjects"]:
     for geom in j2['CityObjects'][each]['geometry']:
-      update_vertex_index(geom["boundaries"], oldnewids, j["vertices"], newvertices)
+      update_array_indices(geom["boundaries"], oldnewids, j["vertices"], newvertices)
   j2["vertices"] = newvertices
 
   # TODO : how to deal with BuildingParts for instance? keep in the same file?
@@ -53,6 +52,19 @@ def main():
     j2["appearance"] = {}
     j2["appearance"]["materials"] = newmats
 
+  #-- textures
+  dOldNewIDs = {}
+  newtextures = []
+  for each in j2["CityObjects"]:
+    for geom in j2['CityObjects'][each]['geometry']:
+      if "texture" in geom:
+        print "texture"
+        for each in geom["texture"]:
+          if 'values' in geom["texture"][each]:
+            update_array_indices_textures(geom["texture"][each]['values'], dOldNewIDs, j["appearance"]["textures"], newtextures)
+  # if len(newtextures) > 0:
+  #   j2["appearance"] = {}
+  #   j2["appearance"]["materials"] = newtextures
 
   #-- save sliced CityJSON file
   json_str = json.dumps(j2, indent=2)
@@ -63,8 +75,25 @@ def main():
   # json_str = json.dumps(cm, indent=2)
   # print "\nDone, output written to:", outname
 
+def update_array_indices_textures(a, dOldNewIDs, oldarray, newarray):
+  for i, each in enumerate(a):
+    if isinstance(each, list):
+      update_array_indices_textures(each, dOldNewIDs, oldarray, newarray)
+    elif each is not None:
+      if i == 0:
+        print each
+      # if each in dOldNewIDs:
+      #   a[i] = dOldNewIDs[each]
+      # else:
+      #   a[i] = len(newarray)
+      #   dOldNewIDs[each] = len(newarray)
+      #   newarray.append(oldarray[each])      
+
 
 def update_array_indices(a, dOldNewIDs, oldarray, newarray):
+  #-- a must be an array
+  #-- issue with passing integer is that it's non-mutable, thus can't update
+  #-- (or I don't know how...)
   for i, each in enumerate(a):
     if isinstance(each, list):
       update_array_indices(each, dOldNewIDs, oldarray, newarray)
@@ -75,20 +104,6 @@ def update_array_indices(a, dOldNewIDs, oldarray, newarray):
         a[i] = len(newarray)
         dOldNewIDs[each] = len(newarray)
         newarray.append(oldarray[each])      
-
-
-def update_vertex_index(a, oldnewids, oldvertices, newvertices):
-  for i, each in enumerate(a):
-    if isinstance(each, list):
-      update_vertex_index(each, oldnewids, oldvertices, newvertices)
-    else:
-      if each in oldnewids:
-        a[i] = oldnewids[each]
-      else:
-        a[i] = len(newvertices)
-        oldnewids[each] = len(newvertices)
-        newvertices.append(oldvertices[each])
-
 
 
 if __name__ == "__main__":
