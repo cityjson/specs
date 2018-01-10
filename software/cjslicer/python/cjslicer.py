960daf2767df410eef7f8802a65411ce7c071453
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-01-10 15:16:11 +0100

    WIP cjslicer: vertices, materials, textures work

--- software/cjslicer/python/cjslicer.py
+++ software/cjslicer/python/cjslicer.py
@@ -25,11 +25,13 @@ def main():
   newvertices = []    
   for each in j2["CityObjects"]:
     for geom in j2['CityObjects'][each]['geometry']:
-      update_array_indices(geom["boundaries"], oldnewids, j["vertices"], newvertices)
+      update_array_indices(geom["boundaries"], oldnewids, j["vertices"], newvertices, -1)
   j2["vertices"] = newvertices
 
   # TODO : how to deal with BuildingParts for instance? keep in the same file?
 
+  if "appearance" in j:
+    j2["appearance"] = {}
   #-- materials
   dOldNewIDs = {}
   newmats = []
@@ -47,12 +49,11 @@ def main():
               dOldNewIDs[v] = len(newmats)
               newmats.append(j["appearance"]["materials"][v])      
           if 'values' in geom["material"][each]:
-            update_array_indices(geom["material"][each]['values'], dOldNewIDs, j["appearance"]["materials"], newmats)
+            update_array_indices(geom["material"][each]['values'], dOldNewIDs, j["appearance"]["materials"], newmats, -1)
   if len(newmats) > 0:
-    j2["appearance"] = {}
     j2["appearance"]["materials"] = newmats
 
-  #-- textures
+  #-- textures references (first int in the arrays)
   dOldNewIDs = {}
   newtextures = []
   for each in j2["CityObjects"]:
@@ -61,10 +62,22 @@ def main():
         print "texture"
         for each in geom["texture"]:
           if 'values' in geom["texture"][each]:
-            update_array_indices_textures(geom["texture"][each]['values'], dOldNewIDs, j["appearance"]["textures"], newtextures)
-  # if len(newtextures) > 0:
-  #   j2["appearance"] = {}
-  #   j2["appearance"]["materials"] = newtextures
+            update_array_indices(geom["texture"][each]['values'], dOldNewIDs, j["appearance"]["textures"], newtextures, 0)
+  if len(newtextures) > 0:
+    j2["appearance"]["textures"] = newtextures
+  #-- textures vertices references (1+ int in the arrays)
+  dOldNewIDs = {}
+  newtextures = []
+  for each in j2["CityObjects"]:
+    for geom in j2['CityObjects'][each]['geometry']:
+      if "texture" in geom:
+        print "texture"
+        for each in geom["texture"]:
+          if 'values' in geom["texture"][each]:
+            update_array_indices(geom["texture"][each]['values'], dOldNewIDs, j["appearance"]["vertices-texture"], newtextures, 1)
+  if len(newtextures) > 0:
+    j2["appearance"]["vertices-texture"] = newtextures
+
 
   #-- save sliced CityJSON file
   json_str = json.dumps(j2, indent=2)
@@ -75,35 +88,23 @@ def main():
   # json_str = json.dumps(cm, indent=2)
   # print "\nDone, output written to:", outname
 
-def update_array_indices_textures(a, dOldNewIDs, oldarray, newarray):
-  for i, each in enumerate(a):
-    if isinstance(each, list):
-      update_array_indices_textures(each, dOldNewIDs, oldarray, newarray)
-    elif each is not None:
-      if i == 0:
-        print each
-      # if each in dOldNewIDs:
-      #   a[i] = dOldNewIDs[each]
-      # else:
-      #   a[i] = len(newarray)
-      #   dOldNewIDs[each] = len(newarray)
-      #   newarray.append(oldarray[each])      
 
-
-def update_array_indices(a, dOldNewIDs, oldarray, newarray):
+def update_array_indices(a, dOldNewIDs, oldarray, newarray, slicearray):
+  #-- slicearray: -1=none ; 0=use-only-first (for textures) ; 1=use-1+ (for textures)
   #-- a must be an array
   #-- issue with passing integer is that it's non-mutable, thus can't update
   #-- (or I don't know how...)
   for i, each in enumerate(a):
     if isinstance(each, list):
-      update_array_indices(each, dOldNewIDs, oldarray, newarray)
+      update_array_indices(each, dOldNewIDs, oldarray, newarray, slicearray)
     elif each is not None:
-      if each in dOldNewIDs:
-        a[i] = dOldNewIDs[each]
-      else:
-        a[i] = len(newarray)
-        dOldNewIDs[each] = len(newarray)
-        newarray.append(oldarray[each])      
+      if ( (slicearray == -1) or (slicearray == 0 and i == 0) or (slicearray == 1 and i > 0) ):
+        if each in dOldNewIDs:
+          a[i] = dOldNewIDs[each]
+        else:
+          a[i] = len(newarray)
+          dOldNewIDs[each] = len(newarray)
+          newarray.append(oldarray[each])      
 
 
 if __name__ == "__main__":
