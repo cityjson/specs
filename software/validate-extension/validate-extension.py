7ce6c023d8c484a4986715433430036d1b2b481b
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-01-28 10:48:04 +0100

    script to validate Extensions is now generic, and not path to my computer...

--- software/validate-extension/validate-extension.py
+++ software/validate-extension/validate-extension.py
@@ -8,11 +8,12 @@ import jsonref
 schema = '../../schema/v09/extensions/extension.schema.json'
 js = json.loads(open(schema).read())
 
-dataset = '/Users/hugo/projects/cityjson/schema/v09/extensions/misc.json'
+dataset = '../../schema/v09/extensions/misc.json'
 j = json.loads(open(dataset).read())
 
 try:
     jsonschema.validate(j, js)
+    print ("All good!")
 except jsonschema.ValidationError as e:
     print ("ERROR:   ", e.message)
 except jsonschema.SchemaError as e:
