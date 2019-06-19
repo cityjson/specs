355e949fe6438adeefc9baf9583aa0c53157f696
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-19 16:00:27 +0200

    Modified the minifier to have no spaces/tabs
    2-step operation seems necessary because bugs in jsonref... this is shit

--- misc/schema-minifier.py
+++ misc/schema-minifier.py
@@ -7,14 +7,23 @@ import jsonref
 root_schema = os.path.abspath('../schemas/cityjson.schema.json')
 fins = open(root_schema)
 
+
 abs_path = os.path.abspath(os.path.dirname(root_schema))
 base_uri = 'file://{}/'.format(abs_path)
 
 js = jsonref.loads(fins.read(), jsonschema=True, base_uri=base_uri)
 
+# a = jsonref.replace_refs()
+
 # -- output stitched_schema
-# json_str = jsonref.dumps(js, indent=2)
-json_str = jsonref.dumps(js, separators=(',',':'))
-opath = os.path.abspath('../schemas/cityjson.min.schema.json')
+json_str = jsonref.dumps(js, indent=2)
+# json_str = jsonref.dumps(js, separators=(',',':')) #-- this doesn't work: WTF
+opath = os.path.abspath('temp.json')
 f = open(opath, "w")
 f.write(json_str)
+
+f2 = open('./temp.json')
+j2 = json.loads(f2.read())
+json_str2 = json.dumps(j2, separators=(',',':'))
+f = open("../schemas/cityjson.min.schema.json", "w")
+f.write(json_str2)
